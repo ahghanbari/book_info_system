0760338c3d8c62433a3bdb0dab10a5eee7c65b51
arash.ph <arash.ph17@gmail.com>
2021-07-07 14:55:54 +0430

    modified: book_store/models writing Book class, add .gitignore

--- book_store/models.py
+++ book_store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=400)
+    pub_date = models.DateField()
+    pages_number = models.IntegerField()
+    price = models.IntegerField()
+    description = models.TextField()
+
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.title
