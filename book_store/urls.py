b0b7b02cda3de17403be46ae58d664cfc2f66052
arash.ph <arash.ph17@gmail.com>
2021-07-09 11:57:18 +0430

    add this new features: adding new book, editing book, deleting book

--- book_store/urls.py
+++ book_store/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import (
+    BookListView, 
+    BookDetailView, 
+    BookCreateView, 
+    BookUpdateView, 
+    BookDeleteView 
+)
 
 urlpatterns = [
+    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
+    path('book/<int:pk>/edit/', BookUpdateView.as_view(), name='book_edit'),
+    path('book/new/', BookCreateView.as_view(), name='book_new'),
     path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('', BookListView.as_view(), name='home')
 ]
--- book_store/views.py
+++ book_store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Book
 
 class BookListView(ListView):
@@ -9,3 +11,19 @@ class BookListView(ListView):
 class BookDetailView(DetailView):
     model = Book
     template_name = 'book_detail.html'
+
+class BookCreateView(CreateView):
+    model = Book
+    template_name = 'book_new.html'
+    fields = '__all__'
+
+class BookUpdateView(UpdateView):
+    model = Book
+    template_name = 'book_edit.html'
+    fields = ['title', 'pub_date', 'pages_number', 'price', 'description']
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'book_delete.html'
+    success_url = reverse_lazy('home')
