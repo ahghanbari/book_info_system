f96a7047c1ad5775c0e6c4f11de82a587c07cb8c
arash.ph <arash.ph17@gmail.com>
2021-07-09 10:02:11 +0430

    writing view: BookList and BookDetail class

--- book_store/views.py
+++ book_store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book
 
-# Create your views here.
+class BookListView(ListView):
+    model = Book
+    template_name = 'home.html'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
