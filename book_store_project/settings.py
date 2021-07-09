f09f7f5fbd30fb11b4701188c9d199164014cc46
arash.ph <arash.ph17@gmail.com>
2021-07-09 15:48:52 +0430

    develop sign up and change css

--- book_store_project/settings.py
+++ book_store_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'book_store.apps.BookStoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
